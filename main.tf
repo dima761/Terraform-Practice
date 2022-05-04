f996df32775b7e7762d49b8056545209c5ce42f6
Dima <dima761@gmail.com>
2022-05-04 14:32:51 +0300

    Added security group to allow traffic to port 8080

--- main.tf
+++ main.tf
@@ -5,7 +5,22 @@ provider "aws" {
 resource "aws_instance" "test" {
   ami = "ami-0022f774911c1d690"
   instance_type = "t2.micro"
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
    tags = {
      Name = "my-ec2-instance"
    }
+resource "aws_security_group" "instance" {
+  name = "terrafom-example-instance"
+  
+
+  ingress {
+    from_port    = 8080
+    to_port      = 8080
+    protocol     = "tcp"
+    cidr_blocks  = ["0.0.0.0/0"]
+  }
 }
